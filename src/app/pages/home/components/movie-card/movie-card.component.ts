e826b39aa754d52cda522d3443c0162b86a9b414
JJatau <jjatau14@outlook.com>
2024-02-02 00:16:15 +0000

    first commit

---
+++ src/app/pages/home/components/movie-card/movie-card.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { Movie, Movies } from '../../../../models/movie.model';
+
+@Component({
+  selector: 'app-movie-card',
+  templateUrl: './movie-card.component.html',
+  styleUrl: './movie-card.component.css',
+})
+export class MovieCardComponent {
+  @Input() card!: Movie[];
+}
